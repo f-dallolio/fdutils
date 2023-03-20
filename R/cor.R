fb90ba7a68ec18043a54604c8b6336eb475c1c3e
Filippo Dall'Olio <fdallolio@brocku.ca>
2023-03-20 01:45:27 -0400

    fix cor and pairwise

--- R/cor.R
+++ R/cor.R
@@ -8,35 +8,39 @@
 #'
 #' @examples
 #'
-cor <- function(x,
-                    out_list = FALSE,
-                    original = FALSE,
-                    use = "complete.obs",
-                    ...)
+cor <- function (x,
+                 out_list = FALSE,
+                 original = FALSE,
+                 use = "complete.obs",
+                 ...)
 {
-  stopifnot("x must be matrix/dataframe/tibble with NCOL > 1" = NCOL(x) > 1)
+  stopifnot(`x must be matrix/dataframe/tibble with NCOL > 1` =
+              NCOL(x) > 1)
+
   no_tibble <- !tibble::is_tibble(x)
-  if(no_tibble) {
+
+  if (no_tibble) {
     .tbl <- tibble::as_tibble(x)
   } else {
     .tbl <- x
   }
+
   nms <- colnames(.tbl)
+
   cor_mat <- stats::cor(.tbl)
-  if(original){
+
+  if (original) {
     return(cor_mat)
   }
-  pair_tbl <- make_pairwise(.tbl) |>
-    rlang::set_names(c("x1", "x2"))
-  out <- pair_tbl |>
-    dplyr::mutate(
-      cor = purrr::map2_dbl(
-        .x = x1,
-        .y = x2,
-        .f = ~ cor_mat[.x, .y]),
-      x1 = colnames(.tbl)["x1"],
-      x2 = colnames(.tbl)["x2"],
-    )
-  if(out_list){ return(as.list(out)) }
+
+  pair_tbl <- rlang::set_names(make_pairwise(.tbl), c("V1",
+                                                      "V2"))
+  out <- dplyr::mutate(pair_tbl, cor = purrr::map2_dbl(.x = V1,
+                                                       .y = V2, .f = ~cor_mat[.x, .y]),
+                       V1 = colnames(.tbl)[V1],
+                       V2 = colnames(.tbl)[V2])
+  if (out_list) {
+    return(as.list(out))
+  }
   out
 }
--- R/make_pairwise.R
+++ R/make_pairwise.R
@@ -8,18 +8,22 @@
 #' @export
 #'
 #' @examples
-make_pairwise <- function(x, out_list  = FALSE)
+make_pairwise <- function (x, out_list = FALSE)
 {
-  stopifnot("x must be matrix/dataframe/tibble with NCOL > 1" = NCOL(x) > 1)
+  stopifnot(`x must be matrix/dataframe/tibble with NCOL > 1` = NCOL(x) >
+              1)
   no_tibble <- !(tibble::is_tibble(x))
-  if(no_tibble) {
+  if (no_tibble) {
     x <- tibble::as_tibble(x)
   }
-  seq_cols <- 1 : NCOL(x)
-  out <-tidyr::expand_grid(seq_cols, seq_cols)
-  # |>
-  #   rlang::set_names(nm = c("x1", "x2")) |>
-  #   dplyr::filter(x1 < x2)
-  if(out_list) {as.list(out)}
-  else {out}
+  seq_cols <- 1:NCOL(x)
+  out <- tidyr::expand_grid(V1 = seq_cols, V2 = seq_cols) %>%
+    group_by(V1) %>%
+    filter(V2 > V1)
+  if (out_list) {
+    as.list(out)
+  }
+  else {
+    out
+  }
 }
