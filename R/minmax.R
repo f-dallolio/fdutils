7191bc4f53e22cc82ff13de3a561629967e927c9
Filippo Dall'Olio <fdallolio@brocku.ca>
2023-03-26 13:54:26 -0400

    .

--- R/minmax.R
+++ R/minmax.R
@@ -14,7 +14,7 @@ minmax <- function(x, min, max, x100 = FALSE, alpha = 0) {
   alpha1 = 1 - (alpha)
   x <- x[!is.na(x)]
   med <- median(x)
-  out <-  (((x - med - .1) * alpha1 + med + .1)  / (max - min))
+  out <-  (((x - med) * alpha1 + med)  / (max - min))
   if(x100){
     return(out * 100)
   }
