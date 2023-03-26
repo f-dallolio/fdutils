f0350c890118d76346b7dccd9622547ccbe497d2
Filippo Dall'Olio <fdallolio@brocku.ca>
2023-03-26 14:36:01 -0400

    .

--- R/minmax.R
+++ R/minmax.R
@@ -10,10 +10,15 @@
 #' @export
 #'
 #' @examples
-minmax <- function(x, min, max, x100 = FALSE, alpha = 0) {
+minmax <- function(x, min = NULL, max = NULL, x100 = FALSE, alpha = 0) {
   alpha1 = 1 - alpha
-  # x <- x[!is.na(x)]
-  med <- median(x, na.rm = TRUE)
+  if(is.null(min)){
+    min <- min(x, na.rm = TRUE)
+  }
+  if(is.null(max)){
+    max <- max(x, na.rm = TRUE)
+  }
+  med <- quantile(x, 0.5, na.rm = TRUE)
   out <-  (((x - med) * alpha1 + med)  / (max - min))
   if(x100){
     return(out * 100)
