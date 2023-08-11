7d8b6a8833e9360d5e39699de445455880068a8a
Filippo DallOlio <fdallolio@brocku.ca>
2023-08-11 10:39:23 -0400

    fixed %notin%

--- R/not_in.R
+++ R/not_in.R
@@ -9,17 +9,9 @@
 #' @examples
 #' x <- letters[1:5]
 #' y <- c("4",2,"a")
-
-x %notin% y
-y %notin% x
+#' x %notin% y
+#' y %notin% x
 
 `%notin%` <- function(x, y){
   !(x %in% y)
 }
-
-
-x <- letters[1:5]
-y <- c("4",2,"a")
-
-x %notin% y
-y %notin% x
