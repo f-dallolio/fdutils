2c1d0c50b0eb3fec497d9b9639864be96cbefabc
Filippo Dall'Olio <fdallolio@brocku.ca>
2023-08-15 13:47:17 -0400

    .

--- R/na_summary.R
+++ R/na_summary.R
@@ -1,8 +1,33 @@
-na_summary <- function(x, type = c('n_missing', 'n_complete', 'rate_missing', 'rate_complete')) {
-
-  types = c('n_missing', 'n_complete', 'rate_missing', 'rate_complete')
-  stopifnot("type should be one of c('n_missing', 'n_complete', 'rate_missing', 'rate_complete')" = ! type %in% types)
-
-
-
+#' Summary of Missing Values
+#'
+#' @param x a vector.
+#'
+#' @return a number.
+#' @export
+#'
+#' @examples
+#' x <- c(1,2,3,NA,5,NA,7,8,9,10)
+#' n_missing(x)
+#' n_complete(x)
+#' missing_pct(x)
+#' complete_pct(x)
+#'
+n_missing <- function (x) {
+  sum(is.na(x) | is.null(x))
 }
+
+#' @export
+n_complete <- function (x) {
+  sum(!is.na(x) & !is.null(x))
+}
+
+#' @export
+missing_pct <- function (x){
+  n_missing(x)/length(x)
+}
+
+#' @export
+complete_pct <- function (x){
+  1 - n_missing(x)/length(x)
+}
+
---
+++ R/not_in.R
@@ -0,0 +1,15 @@
+#' Not in
+#'
+#' @param x
+#' @param y
+#'
+#' @return
+#' @export
+#'
+#' @examples
+not_in <- function(x, y){
+  !(x %in% y)
+}
+
+
+
