92e30c80646d49eacc10333b8e113dc3d83ec3e5
Filippo DallOlio <fdallolio@brocku.ca>
2023-08-15 15:37:01 -0400

    .

--- R/str_upper_split.R
+++ R/str_upper_split.R
@@ -3,18 +3,27 @@
 #' @param string the string to modify
 #' @param sep a string indicating the separator.
 #' @param to_lower TRUE or FALSE
+#' @param exceptions list with exceptions.
 #'
 #' @return a string
 #' @export
 #'
 #' @examples
 
-str_upper_split <- function(string, sep = "_", to_lower = TRUE){
-  out <- gsub('([[:upper:]])', stringr::str_c(sep, '\\1'), string) |>
-    stringr::str_sub(2,-1)
-  if(to_lower){
-    return(out |> tolower())
-  } else{
-    return(out)
+str_upper_split <- function (string, sep = "_",  to_lower = TRUE, exceptions = NULL){
+  out <- gsub(pattern = "([[:upper:]])",
+              replacement = " \\1",
+              x = string) %>%
+    str_squish() %>%
+    str_replace_all(pattern = " ",
+                    replacement = sep)
+  if (to_lower) {
+    out <- tolower(out)
   }
+  if(!is.null(exceptions)){
+    for(i in seq_along(exceptions)){
+      out <- out %>% str_replace_all(names(exceptions)[[i]], exceptions[[i]])
+    }
+  }
+  out
 }
