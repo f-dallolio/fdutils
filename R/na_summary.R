5d45dbf3a9df49a37a4c9bbbe7e33778f5eb8ed2
Filippo DallOlio <fdallolio@brocku.ca>
2023-08-15 13:33:56 -0400

    .

---
+++ R/na_summary.R
@@ -0,0 +1,8 @@
+na_summary <- function(x, type = c('n_missing', 'n_complete', 'rate_missing', 'rate_complete')) {
+
+  types = c('n_missing', 'n_complete', 'rate_missing', 'rate_complete')
+  stopifnot("type should be one of c('n_missing', 'n_complete', 'rate_missing', 'rate_complete')" = ! type %in% types)
+
+
+
+}
